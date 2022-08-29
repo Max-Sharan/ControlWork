fee3b0a8456e9ce848d33adcbdfa7068b1941c5c
Max Sharan <qshark55@gmail.com>
2022-08-30 02:44:31 +0300

    вывод исходного массива

---
+++ задача4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string[] array = new string[n];
+
+Console.WriteLine("Введите элементы массива: ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = Console.ReadLine();
+}
+Console.WriteLine("Исходный массив: [ " + string.Join(", ", array) + " ]");
+
