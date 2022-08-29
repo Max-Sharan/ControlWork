7aa31d09c39a58facd49a8168a8bdb0e8e8ddeae
Max Sharan <qshark55@gmail.com>
2022-08-30 02:45:47 +0300

    счетчик элементов с 3 и менее символами

--- задача4/Program.cs
+++ задача4/Program.cs
@@ -9,3 +9,10 @@ for (int i = 0; i < array.Length; i++)
 }
 Console.WriteLine("Исходный массив: [ " + string.Join(", ", array) + " ]");
 
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+        count++;
+}
